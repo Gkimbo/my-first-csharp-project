89f014e9f76b59472f201a913498e1876b92ef7f
Gavin Kimball <you@example.com>
2023-09-27 13:28:10 -0400

    arrays in csharp

--- Program.cs
+++ Program.cs
@@ -129,10 +129,72 @@ string[] fraudulentOrderIDs = {
     "B179"
 };
 
-foreach (string order in fraudulentOrderIDs)
+// foreach (string order in fraudulentOrderIDs)
+// {
+//     if (order.StartsWith("B"))
+//     {
+//         Console.WriteLine(order);
+//     }
+// }
+
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     // Get a random value that equates to ASCII letters A through E
+//     int prefixValue = random.Next(65, 70);
+//     // Convert the random value into a char, then a string
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     // Create a random number, pad with zeroes
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     // Combine the prefix and suffix together, then assign to current OrderID
+//     orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+/*
+    The following code creates five random OrderIDs
+    to test the fraud detection process.  OrderIDs 
+    consist of a letter from A to E, and a three
+    digit number. Ex. A123.
+*/
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if (order.StartsWith("B"))
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+/*The following code converts a string to an array of the characters, reverses the char,
+counts the amount of 'o' appear in the new array, converts it back to a string and prints it out.*/
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+int letterCount = 0;
+
+foreach (char i in message)
+{
+    if (i == 'o')
     {
-        Console.WriteLine(order);
+        letterCount++;
     }
 }
+string newMessage = new String(message);
+
+Console.WriteLine(message);
+Console.WriteLine($"'o' appears {letterCount} times.");
